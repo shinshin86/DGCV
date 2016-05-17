02edea01056238b0729da5f4da6e266a5c0d5b24
shinshin86 <beagles1986@gmail.com>
2016-05-18 06:41:22 +0900

    Create a dgcv.py

---
+++ dgcv.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from git import *
+import time
+
+
+"""Find a this day's the last commit.
+    
+
+Args:
+    item - git commit's data
+
+Returns:
+    this day in last commit data
+
+"""
+def find_last_commit_time(items):
+    
+    return uniq_commit_data
+
+
+def checkout_and_copy(uniq_commit_data, repo, copy_path):
+
+
+
+
+repo = Repo(''' Your Repository is here. ''')
+uniq_commit_days = {}
+uniq_commit_days = find_last_commit_time(repo.iter_commits('master', max_count=100))
+
+
+
+
+
